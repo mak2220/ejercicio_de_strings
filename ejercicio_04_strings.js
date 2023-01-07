4fa21794a80f933f575ee624f1afb59fa469fd8b
mak2220 <113451174+mak2220@users.noreply.github.com>
2023-01-07 12:10:59 -0300

    Add files via upload

---
+++ ejercicio_04_strings.js
@@ -0,0 +1,20 @@
+const nombre = "Marcos";
+
+const apellido = "Berta";
+
+const estudiante = `${nombre} ${apellido}`;
+
+const estudianteMayus = estudiante.toUpperCase();
+
+const estudianteMinus = estudiante.toLowerCase();
+
+const cantidadLetras = estudiante.length;
+
+const primeraNombre = nombre.charAt(0);
+
+const ultimaApellido = apellido.slice(-1);
+
+const estudianteOutSpaces = estudiante.replace(/ /g ,"");
+
+const esta = estudiante.includes(nombre);
+
